ebb4ec722f5942376cc4ad365ac2e63fcbc6e25b
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-15 09:08:33 +0200

    1st commit

---
+++ src/screens/Boilerplate.js
@@ -0,0 +1,13 @@
+
+import {View, Text, StyleSheet} from 'react-native'
+
+const Boilerplate = () => {
+  return (
+    <View>
+        <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
